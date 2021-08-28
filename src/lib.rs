23fbb11ac65e916647ee0468e713161ef52e6384
Jeff Walden <jwalden@mit.edu>
2021-08-27 23:32:53 -0700

    Reformat everything.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 extern crate proc_macro;
-use proc_macro::{TokenStream};
-use quote::{quote};
+use proc_macro::TokenStream;
+use quote::quote;
 
-use unicode_info::derived_core_properties;
 use unicode_info::code_point_table;
+use unicode_info::derived_core_properties;
 
 #[proc_macro]
 pub fn generate_unicode_tables(_input: TokenStream) -> TokenStream {
@@ -19,8 +19,8 @@ pub fn generate_unicode_tables(_input: TokenStream) -> TokenStream {
         static id_start_count: usize = #starts_len;
         static id_continue_count: usize = #continues_len;
     );
-    
+
     code_point_table::generate_code_point_table();
-    
+
     TokenStream::from(code)
 }
--- tests/generate_unicode_tables_macro.rs
+++ tests/generate_unicode_tables_macro.rs
@@ -4,8 +4,8 @@ generate_unicode_tables!();
 
 #[test]
 fn check_generate_unicode_tables() {
-  // These constants were derived not from testing and finding out, but from
-  // comments at end of the respective sections in DerivedCoreProperties.txt.
-  assert_eq!(id_start_count, 131_482);
-  assert_eq!(id_continue_count, 134_434);
+    // These constants were derived not from testing and finding out, but from
+    // comments at end of the respective sections in DerivedCoreProperties.txt.
+    assert_eq!(id_start_count, 131_482);
+    assert_eq!(id_continue_count, 134_434);
 }
